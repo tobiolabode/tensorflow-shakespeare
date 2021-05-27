49054682fa9ec45739b0977436ce93c431673e5f
Tobi Olabode <tobi@tobiolabode.com>
2021-05-27 21:25:33 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-python -m tensorshake.translate.translate \
-       --num_layers 2 \
-       --size 256 \
-       --steps_per_checkpoint 50 \
-       --learning_rate 0.01 \
-       --learning_rate_decay_factor 0.9 \
-       --train_dir /cache \
-       --decode 1 # turn on for prediction
