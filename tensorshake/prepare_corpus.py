6205e95999cb02dd11fd485088e949574b953443
Tobi Olabode <tobi@tobiolabode.com>
2021-05-27 21:22:57 +0100

    Add files via upload

--- tensorshake/prepare_corpus.py
+++ tensorshake/prepare_corpus.py
@@ -13,7 +13,7 @@ from unidecode import unidecode
 from nltk import word_tokenize
 
 from tensorshake import CACHE_DIR
-from tensorshake.translate.data_utils import create_vocabulary, data_to_token_ids
+from tensorshake.translate.data_utils_v2 import create_vocabulary, data_to_token_ids
 from tensorshake.get_data import MODERN_FILENAME, ORIGINAL_FILENAME, TRAIN_SUFFIX, DEV_SUFFIX
 from tensorshake.get_data import MODERN_PATH, ORIGINAL_PATH, MODERN_TRAIN_PATH, MODERN_DEV_PATH, ORIGINAL_TRAIN_PATH, ORIGINAL_DEV_PATH
 
@@ -45,10 +45,10 @@ def _tokenizer(sentence):
     return [w for w in words if w]
 
 
-def tokenizer(sentence): # TODO: not working for apostrophes
+def tokenizer(sentence):  # TODO: not working for apostrophes
     sentence = sentence.strip().lower()
-    if type(sentence) != unicode:
-        sentence = unicode(sentence, encoding='utf-8', errors='replace')
+    if type(sentence) != str:
+        sentence = str(sentence, encoding='utf-8', errors='replace')
     sentence = unidecode(sentence)
     sentence = sentence.replace("' ", "'")
     return word_tokenize(sentence)
@@ -58,20 +58,23 @@ def build_vocab():
     create_vocabulary(MODERN_VOCAB_PATH, MODERN_PATH, MODERN_VOCAB_MAX, tokenizer=tokenizer)
     create_vocabulary(ORIGINAL_VOCAB_PATH, ORIGINAL_PATH, ORIGINAL_VOCAB_MAX, tokenizer=tokenizer)
 
-    print( subprocess.check_output(['wc', '-l', MODERN_VOCAB_PATH]) )
-    print( subprocess.check_output(['wc', '-l', ORIGINAL_VOCAB_PATH]) )
+    print(subprocess.check_output(['wc', '-l', MODERN_VOCAB_PATH]))
+    print(subprocess.check_output(['wc', '-l', ORIGINAL_VOCAB_PATH]))
 
 
 def build_ids():
-    data_to_token_ids(MODERN_TRAIN_PATH, MODERN_TRAIN_IDS_PATH, MODERN_VOCAB_PATH, tokenizer=tokenizer)
+    data_to_token_ids(MODERN_TRAIN_PATH, MODERN_TRAIN_IDS_PATH,
+                      MODERN_VOCAB_PATH, tokenizer=tokenizer)
     data_to_token_ids(MODERN_DEV_PATH, MODERN_DEV_IDS_PATH, MODERN_VOCAB_PATH, tokenizer=tokenizer)
-    data_to_token_ids(ORIGINAL_TRAIN_PATH, ORIGINAL_TRAIN_IDS_PATH, ORIGINAL_VOCAB_PATH, tokenizer=tokenizer)
-    data_to_token_ids(ORIGINAL_DEV_PATH, ORIGINAL_DEV_IDS_PATH, ORIGINAL_VOCAB_PATH, tokenizer=tokenizer)
+    data_to_token_ids(ORIGINAL_TRAIN_PATH, ORIGINAL_TRAIN_IDS_PATH,
+                      ORIGINAL_VOCAB_PATH, tokenizer=tokenizer)
+    data_to_token_ids(ORIGINAL_DEV_PATH, ORIGINAL_DEV_IDS_PATH,
+                      ORIGINAL_VOCAB_PATH, tokenizer=tokenizer)
 
-    print( subprocess.check_output(['wc', '-l', MODERN_TRAIN_IDS_PATH]) )
-    print( subprocess.check_output(['wc', '-l', MODERN_DEV_IDS_PATH]) )
-    print( subprocess.check_output(['wc', '-l', ORIGINAL_TRAIN_IDS_PATH]) )
-    print( subprocess.check_output(['wc', '-l', ORIGINAL_DEV_IDS_PATH]) )
+    print(subprocess.check_output(['wc', '-l', MODERN_TRAIN_IDS_PATH]))
+    print(subprocess.check_output(['wc', '-l', MODERN_DEV_IDS_PATH]))
+    print(subprocess.check_output(['wc', '-l', ORIGINAL_TRAIN_IDS_PATH]))
+    print(subprocess.check_output(['wc', '-l', ORIGINAL_DEV_IDS_PATH]))
 
 
 if __name__ == '__main__':
